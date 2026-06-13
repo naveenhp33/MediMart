3f2f4b5249fa6d6fafd991cda34b51ea0ef64c30
naveen <naveenkumarsubramani2603@gmail.com>
2026-06-13 22:21:58 +0530

    Fix Express 5 wildcard route bug

--- backend/server.js
+++ backend/server.js
@@ -489,7 +489,7 @@ app.post("/api/upload/product", upload.single("image"), (req, res) => {
 
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
